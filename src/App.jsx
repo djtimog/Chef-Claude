221c5bede698c12fb92a8ae660d17fff1c9f853a
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2025-12-06 22:46:58 +0100

    feat: initialize React application with main entry point, styles, and Vite configuration
    - Added main.jsx as the entry point for the React application.
    - Created styles.css for global styles including body and header styles.
    - Implemented a zigzag animation for header navigation icons.
    - Set up vite.config.js for Vite with React plugin.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./_component/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div>I love you</div>
+        <h1>Vite + React</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/_component/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import chefRobot from "../assets/chef-robot.png";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#">
+          <img
+            src={chefRobot}
+            alt="A robot chef icon as the logo"
+            srcset={chefRobot}
+          />
+          <h1>Chef Claude</h1>
+        </a>
+      </nav>
+    </header>
+  );
+}
